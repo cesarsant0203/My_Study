e10aff82e74ad72c85d7824b99166c5472870d25
cesarsant0203 <55715914+cesarsant0203@users.noreply.github.com>
2020-05-14 18:15:42 -0500

    Update 05_if-acl.py

--- WebEx/ETW-EPN-SUMM/05_if-acl.py
+++ WebEx/ETW-EPN-SUMM/05_if-acl.py
@@ -1,4 +1,14 @@
-aclNum = int(input("What is the IPv4 ACL number? "))
+def inputNum(message):
+    while True:
+        try:
+            userNum = int(input(message))       
+        except ValueError:
+            print("Not an number! Try again.")
+            continue
+        else:
+            return userNum 
+        break 
+aclNum = inputNum("What is the IPv4 ACL number? ")
 if aclNum >= 1 and aclNum <= 99:
  print("This is a standard IPv4 ACL.")
 elif aclNum >=100 and aclNum <= 199:
