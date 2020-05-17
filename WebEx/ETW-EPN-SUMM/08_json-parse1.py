87e1ff7e74bc65edc8e8a9084f75e619892caca6
cesarsant0203 <55715914+cesarsant0203@users.noreply.github.com>
2020-05-17 11:39:20 -0500

    Full api integration with errors

---
+++ WebEx/ETW-EPN-SUMM/08_json-parse1.py
@@ -0,0 +1,10 @@
+import urllib.parse
+import requests
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+orig = "Washington"
+dest = "Baltimaore"
+key = "WQbpNARDUOrAoCkGJWVbKJsRGGZpmG3a"
+
+url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+json_data = requests.get(url).json()
+print(json_data)
