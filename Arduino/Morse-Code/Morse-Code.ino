5486aadf710a1962a04b306171e5932a35336449
cesarsant0203 <55715914+cesarsant0203@users.noreply.github.com>
2020-05-23 18:21:10 -0500

    Morse Code for arduino.
    Morse Code for arduino.

--- Arduino/Morse-Code/Morse-Code.ino
+++ Arduino/Morse-Code/Morse-Code.ino
@@ -1,6 +1,7 @@
 //morse_code by LED
 /*
   This code was written by Ebrahim Bawazir in 2013 and updated in 2015
+  THis is a modified version of the same code By Cesar Santacruz
           U R FREE TO USE IT AND MODIFY IT AS U WANT
                       *****************
 */
